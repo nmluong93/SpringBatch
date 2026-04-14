be6b45c0ac8920a2c7fe546843c6def5f0ba83a6
luongnguyen <luongnm93@live.com>
2026-04-14 11:15:32 +0700

    Implement JobExecutionListener using annotations: - @BeforeJob - @AfterJob

--- src/main/java/com/springbatch/simplespringbatch/listener/MyJobExecutionListener.java
+++ src/main/java/com/springbatch/simplespringbatch/listener/MyJobExecutionListener.java
@@ -1,19 +1,21 @@
 package com.springbatch.simplespringbatch.listener;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.annotation.AfterJob;
+import org.springframework.batch.core.annotation.BeforeJob;
 import org.springframework.batch.core.job.JobExecution;
 import org.springframework.batch.core.listener.JobExecutionListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class MyJobExecutionListener implements JobExecutionListener {
+public class MyJobExecutionListener /*implements JobExecutionListener*/ {
     /**
      * Callback before a job executes.
      *
      * @param jobExecution the current {@link JobExecution}
      */
-    @Override
+    @BeforeJob
     public void beforeJob(JobExecution jobExecution) {
         log.info("JobExecutionListener:BeforeJob - Job name {} \n\t Parameters: {} - Start: {}",  jobExecution.getJobInstance().getJobName(), jobExecution.getJobParameters(),  jobExecution.getStartTime());
     }
@@ -25,7 +27,7 @@ public class MyJobExecutionListener implements JobExecutionListener {
      *
      * @param jobExecution the current {@link JobExecution}
      */
-    @Override
+    @AfterJob
     public void afterJob(JobExecution jobExecution) {
         log.info("JobExecutionListener:AfterJob - Job name {} \n\t Start {} - End {}",  jobExecution.getJobInstance().getJobName(), jobExecution.getStartTime(), jobExecution.getEndTime());
     }
